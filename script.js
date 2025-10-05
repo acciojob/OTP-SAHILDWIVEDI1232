74aa1c06b13e016dbb6d392981b97637e0e1ba3b
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-10-05 21:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',function(){
+	const inputs=document.querySelectorAll('.code');
+	for(let index=0;index<inputs.length;index++){
+		const input=inputs[index];
+		input.addEventListener('input',function(){
+			if(this.value===1 && index<inputs.length-1){
+				inputs[index+1].focus();
+			}
+		});
+		inputs.addEventListener('keydown',function(event){
+										if(event.key==='Backspace'){
+											if(this.value===0 && index>0){
+												inputs[index-1].focus();
+											}
+										}});
+	}
+	
+	});
+
+
+
+
+	
+})
